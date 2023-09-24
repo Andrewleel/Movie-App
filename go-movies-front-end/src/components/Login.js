7cfc0c07e4d67033b48068e0ec47252da4b49594
Andrew Lee <andrewslee030401@gmail.com>
2023-09-24 16:02:13 -0400

    a lot of changes regarding front-end

--- go-movies-front-end/src/components/Login.js
+++ go-movies-front-end/src/components/Login.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Input from "./form/Input";
 
 const Login = () => {
-  return (
-    <div>
-        <h2>Login</h2>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email == "admin@example.com") {
+        
+    }
+  }
+
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>Login</h2>
+      <hr /> 
+
+      <form onSubmit={ handleSubmit }>
+        <Input 
+            title="Email Address"
+            type="email"
+            className="form-control"
+            name="email"
+            autoComplete="email-new"
+            onChange={(event) => {
+                setEmail(event.target.value)
+            }}
+        />
+        <Input 
+            title="Password"
+            type="password"
+            className="form-control"
+            name="password"
+            autoComplete="password-new"
+            onChange={(event) => {
+                setPassword(event.target.value)
+            }}
+        />
+
+        <hr />
+        <input 
+            type="submit"
+            className="btn btn-primary"
+            value="Login"
+        />
+
+      </form>
+    </div>
+  );
+};
+
+export default Login;
