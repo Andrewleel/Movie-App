90f42603ec733efa1967786352bc91252d49dde0
Andrew Lee <andrewslee030401@gmail.com>
2023-09-24 17:08:05 -0400

    Initialized initial server and imported chi router

--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -1,5 +1,33 @@
-package main 
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+
+}
 
 func main() {
-	
+	// set applications config
+	app := &application{
+		Domain: "example.com",
+	}
+	// read from command line
+
+	// connect to the database
+	log.Println("Starting application on port: ", port)
+
+	http.HandleFunc("/", Hello)
+
+	// start a web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes());
+	if err != nil {
+		log.Fatal(err)
+	}
 }
