7c0be88f6dc47dd9e8adbc6f13f4165ca3c6eb60
Andrew Lee <andrewslee030401@gmail.com>
2023-09-24 16:29:32 -0400

    starting back-end

--- go-movies-front-end/src/components/Login.js
+++ go-movies-front-end/src/components/Login.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import Input from "./form/Input";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { setJwtToken } = useOutletContext();
+  const { setAlertMessage } = useOutletContext();
+  const { setAlertClassName } = useOutletContext();
+
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (email == "admin@example.com") {
-        
+    console.log("email/pass", email, password);
+    if (email === "admin@example.com") {
+        setJwtToken("abc");
+        setAlertClassName("d-none")
+        setAlertMessage("")
+        navigate("/")
+    } else {
+        setAlertClassName("alert-danger")
+        setAlertMessage("Invalid credentials")
     }
   }
 
